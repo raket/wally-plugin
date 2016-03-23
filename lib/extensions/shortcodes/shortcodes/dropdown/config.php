98d51ce500231cc2f41cc4076b57f18194a4b1f6
Filip Hallqvist <filip@raket.nu>
2016-03-23 10:55:17 +0100

    Updated translations

--- lib/extensions/shortcodes/shortcodes/dropdown/config.php
+++ lib/extensions/shortcodes/shortcodes/dropdown/config.php
@@ -4,13 +4,6 @@
 
 $cfg = array();
 
-//$cfg['page_builder'] = array(
-//	'title'       => __( 'Alternativknapp', 'fw' ),
-//	'description' => __( 'Lägg till en knapp med utfällbara alternativ', 'fw' ),
-//	'tab'         => __( 'Content Elements', 'fw' ),
-//	'icon'        => 'unycon unycon-list-ul',
-//);
-
 $cfg['page_builder'] = array(
 		'title'       => __( 'Select button', 'fw' ),
 		'description' => __( 'Add a button with multiple alternatives', 'fw' ),
