31f859ffdaac5b48e87dd747abd7659073d25ffc
Filip Hallqvist <filip@raket.nu>
2016-03-23 14:43:16 +0100

    FIX: main row is now a div instead of section

--- lib/extensions/shortcodes/shortcodes/section/views/view.php
+++ lib/extensions/shortcodes/shortcodes/section/views/view.php
@@ -4,8 +4,8 @@
 $table_class = ( isset( $atts['border'] ) && $atts['border'] ) ? 'border' : '';
 $container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 ?>
-<section class="fw-main-row <?php echo $table_class; ?>">
+<div class="fw-main-row <?php echo $table_class; ?>">
 	<div class="<?php echo $container_class; ?>">
 		<?php echo do_shortcode( $content ); ?>
 	</div>
-</section>
+</div>
